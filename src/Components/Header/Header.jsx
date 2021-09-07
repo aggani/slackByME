a6207122371d3a32a502e234ca988b56300852ab
Anirudh <iamanirudhaggarwal@gmail.com>
2021-09-08 04:53:06 +0530

    Header component added and initiated build

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import './Header.css';
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+
+function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderLeft>
+                <HeaderAvatar 
+                    // TODO Add onClick
+                />
+                <AccessTimeIcon />
+            </HeaderLeft>
+            
+        </HeaderContainer>
+    )
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
+`;
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px;
+    }
+`;
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover{
+        opacity: 0.8;
+    }
+`;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDu2V21mGfNy6rLyzRjg19lUfJZiKhoLTs",
+    authDomain: "slack-clone-a0783.firebaseapp.com",
+    projectId: "slack-clone-a0783",
+    storageBucket: "slack-clone-a0783.appspot.com",
+    messagingSenderId: "556955061636",
+    appId: "1:556955061636:web:a36892e8888480b91321d1"
+  };
